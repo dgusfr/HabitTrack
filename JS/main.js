f1e162072435a1b007f1d429984ff643b014a1b3
dgusfr <diegogustavo.fr@gmail.com>
2024-08-26 10:08:01 -0300

    add : get Element habit list and goal

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("HabitTrack is ready!");
 });
+
+document
+  .getElementById("habit-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const habitName = document.getElementById("habit-name").value;
+    const habitGoal = document.getElementById("habit-goal").value;
+
+    if (habitName && habitGoal) {
+      const habitList = document.getElementById("habits");
+      const newHabit = document.createElement("li");
+      newHabit.innerHTML = `<span class="habit-name">${habitName}</span> <span class="habit-goal">${habitGoal} times/day</span>`;
+      habitList.appendChild(newHabit);
+    }
+
+    document.getElementById("habit-form").reset();
+  });
